f9e3618d6382fc6a477c444f2812b0bd91143ab8
Y. Sakamoto <sakamoto@acroquest.co.jp>
2016-10-01 18:28:10 +0900

    少しSimple化

--- src/main/java/acroquest/java/junit/Greeting.java
+++ src/main/java/acroquest/java/junit/Greeting.java
@@ -1,6 +1,17 @@
 package acroquest.java.junit;
 
+/**
+ * あいさつの言葉を決めるクラス。
+ *
+ * @author acroquest
+ */
 public class Greeting {
+	/**
+	 * あいさつの言葉を取得する。
+	 *
+	 * @param hour 時間
+	 * @return 時間帯に応じたあいさつの言葉
+	 */
     public String getMessage(int hour) {
         String message;
         if (hour >= 5 && hour < 11) {
