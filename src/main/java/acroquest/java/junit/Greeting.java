169a3baa19e3bd99aa0fbd24a59125225e6829d8
Y. Sakamoto <sakamoto@acroquest.co.jp>
2016-10-01 20:19:28 +0900

    メソッドの中身を最終形に修正

--- src/main/java/acroquest/java/junit/Greeting.java
+++ src/main/java/acroquest/java/junit/Greeting.java
@@ -14,12 +14,12 @@ public class Greeting {
 	 */
     public String getMessage(int hour) {
         String message;
-        if (hour >= 5 && hour < 11) {
-            message = "おはようございます";
-        } else if (hour >= 11 && hour < 17) {
-            message = "こんにちは";
-        } else {
+        if (hour >= 17 || hour < 5) {
             message = "こんばんは";
+        } else if (hour < 11) {
+            message = "おはようございます";
+        } else {
+            message = "こんにちは";
         }
         return message;
     }
