5f0891829f3dce0732d9ba033eb3f977e9f4da16
phonypianist <phonypianist@gmail.com>
2017-05-02 23:29:36 +0900

    パッケージ名修正

---
+++ src/main/java/jp/co/acroquest/javabook/junit/Greeting.java
@@ -0,0 +1,26 @@
+package jp.co.acroquest.javabook.junit;
+
+/**
+ * あいさつの言葉を決めるクラス。
+ *
+ * @author acroquest
+ */
+public class Greeting {
+	/**
+	 * あいさつの言葉を取得する。
+	 *
+	 * @param hour 時間
+	 * @return 時間帯に応じたあいさつの言葉
+	 */
+    public String getMessage(int hour) {
+        String message;
+        if (hour >= 17 || hour < 5) {
+            message = "こんばんは";
+        } else if (hour < 11) {
+            message = "おはようございます";
+        } else {
+            message = "こんにちは";
+        }
+        return message;
+    }
+}
